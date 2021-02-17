a944229ecac2545abd95e6f2607307d91a56bf53
Brandon Lefert <brandon.lefert@gmail.com>
2021-02-17 11:56:31 -0600

    Made a main menu with rules and a way to start game.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,33 @@
 *	@brief
 */
 #include <iostream>
+using namespace std;
 int main()
 {
+	bool loop = true;
+	int selection = 0;
+  cout << "  WELCOME TO" << endl;
+  cout << " //BATTLESHIP//" << endl << endl << endl;
+  while(loop)
+  {
+    cout << "Enter in number to navigate menu" << endl;
+    cout << "1: Start Game" << endl;
+    cout << "2: How to Play" << endl;
+    cin >> selection;
+    switch (selection)
+    {
+      case 1:
+        loop = false;
+        break;
+      case 2:
+				cout << "BASIC RULES" << endl;
+			  cout << "The goal of the game is to eliminate all of your opponent's ships ";
+			  cout << "by selecting spots on the game board to see if you hit or miss." << endl;
+			  cout << "The game is over when one player loses all of their ships." << endl;
+        break;
+      default:
+      cout << selection << " is not a valid choice, try again." << endl;
+    }
+  }
 	return (0);
 }
