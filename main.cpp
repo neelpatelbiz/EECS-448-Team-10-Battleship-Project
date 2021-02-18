9e51a5d824775674fb115cf12a1435abdc7b86d2
Brandon Lefert <brandon.lefert@gmail.com>
2021-02-18 15:50:45 -0600

    Fixed errors in main.cpp to allow program to compile.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@
 *	@brief
 */
 #include <iostream>
-#include "Excutive.h"
+#include "Executive.h"
+#include "Board.h"
+#include "Ships.h"
 using namespace std;
 int main()
 {
@@ -42,32 +44,22 @@ int main()
   }
 	int numberOfShips = 0;
 	int row;
-	int column;
+	char column;
 	bool vert;
 	cout << "Enter the number of ships you would like to play with, up to a total of 6." << endl;
 	cin >> numberOfShips;
-	switch(numberOfShips)
-	{
-		case 6:
-			Ship p1Carrier(6);
-			Ship p2Carrier(6);
-		case 5:
-			Ship p1BattleShip(5);
-			Ship p2BattleShip(5);
-		case 4:
-			Ship p1Destroyer(4);
-			Ship p2Destroyer(4);
-		case 3:
-			Ship p1Cruiser(3);
-			Ship p2Cruiser(3);
-		case 2:
-			Ship p1Patrol(2);
-			Ship p2Patrol(2);
-		default:
-			Ship p1Sub(1);
-			Ship p2Sub(1);
-
-	}
+	Ships p1Carrier(6);
+	Ships p2Carrier(6);
+	Ships p1BattleShip(5);
+	Ships p2BattleShip(5);
+	Ships p1Destroyer(4);
+	Ships p2Destroyer(4);
+	Ships p1Cruiser(3);
+	Ships p2Cruiser(3);
+	Ships p1Patrol(2);
+	Ships p2Patrol(2);
+	Ships p1Sub(1);
+	Ships p2Sub(1);
 	cout << "HIDE THE SCREEN SO ONLY ONE PLAYER CAN SEE IT" << endl;
 	cout << "PLAYER 1" << endl;
 	for(int i = 0; i < numberOfShips; i++)
@@ -88,33 +80,35 @@ int main()
 			cout << "Not a valid column position, try again." << endl;
 			cin >> column;
 		}
+		int trueColumn = (int)column;
 		cout << "Enter any value if you want the ship to be oriented vertically, 0 for horizontal." << endl;
 		cin >> vert;
 		if(i == 0)
 		{
-			p1Ships.addShip(p1Sub, row, column, vert, i + 1);
+			p1Ships.addShip(p1Sub, row, trueColumn, vert, i + 1);
 		}
 		else if(i == 1)
 		{
-			p1Ships.addShip(p1Patrol, row, column, vert, i + 1);
+			p1Ships.addShip(p1Patrol, row, trueColumn, vert, i + 1);
 		}
 		else if(i == 2)
 		{
-			p1Ships.addShip(p1Cruiser, row, column, vert, i + 1);
+			p1Ships.addShip(p1Cruiser, row, trueColumn, vert, i + 1);
 		}
 		else if(i == 3)
 		{
-			p1Ships.addShip(p1Destroyer, row, column, vert, i + 1);
+			p1Ships.addShip(p1Destroyer, row, trueColumn, vert, i + 1);
 		}
 		else if(i == 4)
 		{
-			p1Ships.addShip(p1BattleShip, row, column, vert, i + 1);
+			p1Ships.addShip(p1BattleShip, row, trueColumn, vert, i + 1);
 		}
 		else if(i == 5)
 		{
-			p1Ships.addShip(p1Carrier, row, column, vert, i + 1);
+			p1Ships.addShip(p1Carrier, row, trueColumn, vert, i + 1);
 		}
 	}
+	p1Ships.Display();
 	cout << "SWITCH PLAYERS" << endl;
 	cout << "PLAYER 2" << endl;
 	for(int i = 0; i < numberOfShips; i++)
@@ -135,31 +129,32 @@ int main()
 			cout << "Not a valid column position, try again." << endl;
 			cin >> column;
 		}
+		int trueColumn = (int)column;
 		cout << "Enter any value if you want the ship to be oriented vertically, 0 for horizontal." << endl;
 		cin >> vert;
 		if(i == 0)
 		{
-			p2Ships.addShip(p2Sub, row, column, vert, i + 1);
+			p2Ships.addShip(p2Sub, row, trueColumn, vert, i + 1);
 		}
 		else if(i == 1)
 		{
-			p2Ships.addShip(p2Patrol, row, column, vert, i + 1);
+			p2Ships.addShip(p2Patrol, row, trueColumn, vert, i + 1);
 		}
 		else if(i == 2)
 		{
-			p2Ships.addShip(p2Cruiser, row, column, vert, i + 1);
+			p2Ships.addShip(p2Cruiser, row, trueColumn, vert, i + 1);
 		}
 		else if(i == 3)
 		{
-			p2Ships.addShip(p2Destroyer, row, column, vert, i + 1);
+			p2Ships.addShip(p2Destroyer, row, trueColumn, vert, i + 1);
 		}
 		else if(i == 4)
 		{
-			p2Ships.addShip(p2BattleShip, row, column, vert, i + 1);
+			p2Ships.addShip(p2BattleShip, row, trueColumn, vert, i + 1);
 		}
 		else if(i == 5)
 		{
-			p2Ships.addShip(p2Carrier, row, column, vert, i + 1);
+			p2Ships.addShip(p2Carrier, row, trueColumn, vert, i + 1);
 		}
 	}
 
