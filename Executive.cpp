8085406e45a4823d8029b4db30435bd1dbadf78d
Alex <alexwilhelm@ku.edu>
2021-02-18 18:53:23 -0600

    Add display and screen for attacking to executive

--- Executive.cpp
+++ Executive.cpp
@@ -33,17 +33,9 @@ void Executive::run()
 
 	//SAMPLE PROMPTS: ALL OF THESE NEED TO BE CODED INTO THEIR RESPECITVE SPOTS
 	 //ADD code to make actually affect board.
-	cout << "Great, both players' boats have now been placed on the board. Now, it is time to attack the enemy!";
-	cout << endl;
-	cout << endl;
-	int turn = 1;
 
 	//Turn Text with turn counter
-	cout << "PLAYER 1 TURN [" << turn << "]";
-	cout << endl;
-	//stuff happens
-	cout << "PLAYER 2 TURN [" << turn << "]";
-	cout << endl;
+
 
 	//Player ship sunk
 	cout << "YOUR " << "insert ship name" << " SUNK!";
@@ -251,6 +243,101 @@ void Executive::Game()
 		cin >> vert;
 		p2Ships.addShip(row, trueColumn, vert, i + 1);
 		p2Ships.Display();
+		//add in delay time as second player will not see last ship placed.
+		p2Ships.clearScreen();
+		cout << "Great, both players' boats have now been placed on the board. Now, it is time to attack the enemy!";
+		cout << endl << endl;
+
+		std::string ready = "";
+		do
+		{
+			cout << "Are you ready to begin the game? (y/n)";
+			cout << endl;
+			cin >> ready;
+
+		} while (ready != "yes" && ready != "y");
+
+		cout << endl << endl;
+
+		int turn = 1;
+		cout << "PLAYER 1 TURN [" << turn << "]";
+		cout << endl << endl;
+
+		cout << "YOUR SHIPS:";
+		cout << endl << endl;
+
+		p1Ships.Display();
+
+		cout << endl << endl;
+
+		cout << "ENEMY SHIPS:";
+		cout << endl << endl;
+
+		p2Ships.Display();
+		cout << endl << endl;
+		cout << "Enter in the position of where you would like to attack." << endl;
+		cout << "Columns are labeled A-J, and rows are 1-10" << endl;
+		cout << "Enter in the row value." << endl;
+		cin >> row;
+		while (row < 1 || row > 10)
+		{
+			cout << "Not a valid row position, try again." << endl;
+			cin >> row;
+		}
+		cout << "Enter in the column value." << endl;
+		cin >> column;
+		while (column < 'A' || column > 'J')
+		{
+			cout << "Not a valid column position, try again." << endl;
+			cin >> column;
+		}
+		trueColumn = 0;
+		if (column == 'A')
+		{
+			trueColumn = 1;
+		}
+		else if (column == 'B')
+		{
+			trueColumn = 2;
+		}
+		else if (column == 'C')
+		{
+			trueColumn = 3;
+		}
+		else if (column == 'D')
+		{
+			trueColumn = 4;
+		}
+		else if (column == 'E')
+		{
+			trueColumn = 5;
+		}
+		else if (column == 'F')
+		{
+			trueColumn = 6;
+		}
+		else if (column == 'G')
+		{
+			trueColumn = 7;
+		}
+		else if (column == 'H')
+		{
+			trueColumn = 8;
+		}
+		else if (column == 'I')
+		{
+			trueColumn = 9;
+		}
+		else if (column == 'J')
+		{
+			trueColumn = 10;
+		}
+
+		//stuff happens
+		cout << "PLAYER 2 TURN [" << turn << "]";
+		cout << endl;
+
+
 	}
 }
 
