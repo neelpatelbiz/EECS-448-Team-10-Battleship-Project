2e5e7f817969c1cef4bf570dc7091c09698d5591
Tommaster02 <Th+842994>
2021-02-19 17:43:21 -0600

    Documentation for Board and Ships

--- Board.h
+++ Board.h
@@ -5,16 +5,49 @@
 #include <string>
 #include "Ships.h"
 using namespace std;
+/**
+ * draw the borad. deter the ship statues
+ * @author  Team 10
+ * @version 19, Feb 2021
+ */
 
 class Board
 {
 public:
+  /**
+	 * store the element
+	 * @param   grid it is array to store the syboml
+	 */
   Board();
+  /**
+* discontructor. delete array
+* @param   grid array to delete.
+*/
   ~Board();
+  /**
+* show the element in the array
+* @param   grid array to show syboml
+*/
   void Display();
+  /**
+* clean the screen. fill out with space
+* @param n
+*/
   void clearScreen();
+  /**
+* it is action to chekc how to add ship in the borad
+* @param row, column, v, size, type.
+*/
   bool addShip(int, int, int, int, int);
+  /**
+* check the hit of ship , and return it back
+* @param row, column is from last function
+*/
   string checkHit(int, int);
+  /**
+*  update ship action
+* @param u it is string which contain element.
+*/
   void update(int, int, string);
 
 private:
--- Ships.h
+++ Ships.h
@@ -3,15 +3,44 @@
 
 #include <iostream>
 #include <string>
+/**
+ * draw the ship. determine how ship in the board.
+ * @author  Team 10
+ * @version 19, Feb 2021
+ */
 
 class Ships
 {
 public:
+  /**
+ * constructor
+ * @param  nothing
+ */
   Ships();
+  /**
+ * check the state of destory, draw the syboml
+ * @param  s it is symbol of ship
+ */
   void buildShip(int);
+  /**
+* distructor
+* @param  nothing
+*/
   ~Ships();
+  /**
+* get the size form the gamer choice
+* @param  s  of the size from the choice
+*/
   void setSize(int s);
+  /**
+* get the hit form the gamer
+* @param  hits make the number get bigger.
+*/
   void hit();
+  /**
+* compare the hit with size to get the is the ship destory
+* @param  hit, size form the function before
+*/
   bool isDestroyed();
 
 private:
