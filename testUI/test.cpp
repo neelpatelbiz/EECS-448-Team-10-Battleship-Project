7ab56508744e18b964ffcae7cd54e7718c1644de
Neel <neelpatelbiz@gmail.com>
2021-02-27 10:00:12 -0600

    colors

--- testUI/test.cpp
+++ testUI/test.cpp
@@ -4,14 +4,12 @@
 int main()
 {
         using namespace cimg_library;
-        const unsigned char blue[] = { 128,200,255}, red[] = { 255,0,0 }, white[] = { 255,255,255 };
-        
-        
-        unsigned int w = 500;
-        unsigned int h = 500;
+        const unsigned char blue[] = { 128,200,255}, red[] = { 255,0,0 }, white[] = { 255,255,255 }; 
+        unsigned int W = 400, H = 400;
+
 
         CImg<unsigned char> background;
-        background.assign(w, h, 1, 3);
+        background.assign(W, H, 1, 3);
         background.display();
         // const unsigned char bluegreen[] = {85, 78, 255 };
         // CImg<unsigned char> bg(w,h,1,3);
