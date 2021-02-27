80ffd3ed30aca510f5c3e4807a33a16c63472bee
Neel <neelpatelbiz@gmail.com>
2021-02-27 13:19:13 -0600

    draws grid of rectangles

--- testUI/test.cpp
+++ testUI/test.cpp
@@ -5,14 +5,35 @@ int main()
 {
         using namespace cimg_library;
         const unsigned char blue[] = { 128,200,255}, red[] = { 255,0,0 }, white[] = { 255,255,255 }; 
-        unsigned int W = 400, H = 400;
+        unsigned int W = 500, H = 500, horOffset = 100, verOffset = 100, sqWid=W/15, sqHeight=H/15;
+        
+        //create background
+        CImg<unsigned char> background(W, H, 1, 3, 255); 
+        background.assign(64,64,1,3,0).noise(60).draw_plasma().resize(W,H).blur(2).normalize(0,128);
+        //.draw_rectangle(0,0,W-1,H-1,white,1.0f,~0U);
+        for(int i=0;i<10;i++)
+        {
+          for(int j=0;j<10;j++)
+          {
+            background.draw_rectangle((W/10)*i,(W/10)*j, (W/10)*i+sqWid,(W/10)*j+sqHeight, blue);
+          }
+        }
+        
 
-
-        CImg<unsigned char> background;
-        background.assign(W, H, 1, 3);
-        background.display();
+        //create display
+        CImgDisplay disp(background,"CImg Tetris",0,false,true);
+        disp.move((CImgDisplay::screen_width() - disp.width())/2,(CImgDisplay::screen_height() - disp.height())/2);
+        std::getchar();
         
 }
+
+/*
+  The CImgList<T>::iterator type is defined as a CImg<T>*.
+  You may use it like this:
+  CImgList<> list;   // Assuming this image list is not empty
+  for (CImgList<>::iterator it = list.begin(); it<list.end(); ++it) (*it).mirror('x');
+  //if(shipArr[i][j] == //hitcondition){*it.//changestyle}
+*/
 /*
 Button usage
 //hex 1 for left mouse button
