98d6f76b7e5d024551d376b7664e5d10ad81cf70
Neel <neelpatelbiz@gmail.com>
2021-03-01 18:55:22 -0600

    extract number of ships selection from dialog box

--- testUI/test.cpp
+++ testUI/test.cpp
@@ -3,71 +3,74 @@
 
 int main()
 {
-        using namespace cimg_library;
-        const unsigned char gridLines[] = { 128,200,255}, attacked[] = { 255,0,0 }, 
-	      defaultTile[] = { 255,255,255 }; 
-        unsigned int W = 500, H = 500, horOffset = 100, verOffset = 100, sqWid=W/15,
-		     sqHeight=H/15;
-        
-        //create background
+  using namespace cimg_library;
+  const unsigned char gridLines[] = { 128,200,255}, attacked[] = { 255,0,0 }, 
+  defaultTile[] = { 255,255,255 }; 
+  unsigned int W = 500, H = 500;
+  int numberOfShips;
+  
+  //create background
 
-        CImg<unsigned char> background(W, H, 1, 3, 255); 
-        background.assign(64,64,1,3,0).noise(60).draw_plasma().resize(W,H).blur(2).normalize(0,128);
-	/*
-	CImg<unsigned char> attacked(W/15,H/15, 3, 255);
-	CImg<unsigned char> defaultTile(W/15,H/15, 3, 255);
-	CImg<unsigned char> missed(W/15,H/15, 3, 255);
-	CImg<unsigned char> carrier (W/15,H/15, 3, 255);
-	CImg<unsigned char> battleship (W/15,H/15, 3, 255);
-	CImg<unsigned char> cruiser (W/15,H/15, 3, 255);
-	CImg<unsigned char> destroyer (W/15,H/15, 3, 255);
-	CImg<unsigned char> patrol (W/15,H/15, 3, 255);
-	CImg<unsigned char> sub (W/15,H/15, 3, 255);
-	*/
-        if(cimg::dialog("BASIC RULES",
-        "The goal of the game is to eliminate all of your opponent's ships\n"
-        "by selecting spots on the game board to see if you hit or miss.\n"
-        "The game is over when one player loses all of their ships.\n\n"
-        "At the beginning of the game, each player will set up their boats on a 10x10 grid simulating a board.\n"
-        "Do NOT HIT the same coordinates more than one time when picking where to hit an enemy boat.\n"
-        "You will have the choice between playing with 1 to 6 ships that you will place on the board:\n\n"
-        "Games with 1 ship: \nSub: (1x1)\n\n"
-        "2 ships: \nSub: (1x1), Patrol Boat: (1x2)\n\n"
-        "3 ships: \nSub: (1x1), Patrol Boat: (1x2), Cruiser: (1x3)\n\n"
-        "4 ships: \nSub: (1x1), Patrol Boat: (1x2), Cruiser: (1x3), Destroyer: (1x4)\n\n"
-        "5 ships: \nSub: (1x1), Patrol Boat: (1x2), Cruiser: (1x3), Destroyer: (1x4), Battleship: (1x5)\n\n"
-        "6 ships: \nSub: (1x1), Patrol Boat: (1x2), Cruiser: (1x3), Destroyer: (1x4), Battleship: (1x5), Carrier: (1x6)\n\n"
-        "On the board, there is a few symbols that represent ships, hits, misses, and the board itself:\n\n"
-        "'.' = PORTION OF BOARD NOT INTERACTED WITH, 'H'= HIT, 'M' = MISS\n", "Start", "Quit",0,0,0,0,
-        background, true))std::exit(0);
+  CImg<unsigned char> background(W, H, 1, 3, 255); 
+  background.assign(64,64,1,3,0).noise(60).draw_plasma().resize(W,H).blur(2).normalize(0,128);
+  CImg<unsigned char> SettingsSelect(2, 2, 1, 3, 255); 
+  background.assign(64,64,1,3,0).noise(60).draw_plasma().resize(W,H).blur(2).normalize(0,128);
+  /*
+  CImg<unsigned char> attacked(W/15,H/15, 3, 255);
+  CImg<unsigned char> defaultTile(W/15,H/15, 3, 255);
+  CImg<unsigned char> missed(W/15,H/15, 3, 255);
+  CImg<unsigned char> carrier (W/15,H/15, 3, 255);
+  CImg<unsigned char> battleship (W/15,H/15, 3, 255);
+  CImg<unsigned char> cruiser (W/15,H/15, 3, 255);
+  CImg<unsigned char> destroyer (W/15,H/15, 3, 255);
+  CImg<unsigned char> patrol (W/15,H/15, 3, 255);
+  CImg<unsigned char> sub (W/15,H/15, 3, 255);
+  */
+  if(cimg::dialog("BASIC RULES",
+  "The goal of the game is to eliminate all of your opponent's ships\n"
+  "by selecting spots on the game board to see if you hit or miss.\n"
+  "The game is over when one player loses all of their ships.\n\n"
+  "At the beginning of the game, each player will set up their boats on a 10x10 grid simulating a board.\n"
+  "Do NOT HIT the same coordinates more than one time when picking where to hit an enemy boat.\n"
+  "You will have the choice between playing with 1 to 6 ships that you will place on the board:\n\n"
+  "Games with 1 ship: \nSub: (1x1)\n\n"
+  "2 ships: \nSub: (1x1), Patrol Boat: (1x2)\n\n"
+  "3 ships: \nSub: (1x1), Patrol Boat: (1x2), Cruiser: (1x3)\n\n"
+  "4 ships: \nSub: (1x1), Patrol Boat: (1x2), Cruiser: (1x3), Destroyer: (1x4)\n\n"
+  "5 ships: \nSub: (1x1), Patrol Boat: (1x2), Cruiser: (1x3), Destroyer: (1x4), Battleship: (1x5)\n\n"
+  "6 ships: \nSub: (1x1), Patrol Boat: (1x2), Cruiser: (1x3), Destroyer: (1x4), Battleship: (1x5), Carrier: (1x6)\n\n"
+  , "Start", "Quit",0,0,0,0,
+  background, true))std::exit(0);
 
-        
-        //.draw_rectangle(0,0,W-1,H-1,white,1.0f,~0U);
-        for(int i=0;i<10;i++)
-        {
-          for(int j=0;j<10;j++)
-          {
-            //if(p2Ships.checkHit(i+1,j+1) == "S" || 
-            //p2Ships.checkHit(i+1,j+1) == "P" ||
-            //p2Ships.checkHit(i+1,j+1) == "c" ||
-            //p2Ships.checkHit(i+1,j+1) == "D" ||
-            //p2Ships.checkHit(i+1,j+1) == "B" ||
-            //p2Ships.checkHit(i+1,j+1) == "C" ||)
-            background.draw_rectangle((W/12)+(W/12)*j+1,(H/12)*i+(H/6)+1, 
-		        (W/12)*j+(W/6)-1, (H/12)*i+((3*H)/12)-1, attacked);
-            //else if(p2Ships.checkHit(i+1, j+1) == ".")
-            
-            background.draw_rectangle((W/12)+(W/12)*j,(H/12)*i+(H/6), 
-		        (W/12)*j+(W/6), (H/12)*i+((3*H)/12), gridLines, 1, ~0U);
-          }
-        }
-        
+  numberOfShips = cimg::dialog("Number Of Ships","Enter the number of ships you would like to play with, up to a total of 6.", "1", "2","3","4","5","6", background, true) + 1;
 
-        //create display
-        CImgDisplay disp(background,"BattleShip",0,false,true);
-        disp.move((CImgDisplay::screen_width() - disp.width())/2,
-			(CImgDisplay::screen_height() - disp.height())/2);
-        std::getchar();
+  std::cout<<numberOfShips;
+  
+  //.draw_rectangle(0,0,W-1,H-1,white,1.0f,~0U);
+  for(int i=0;i<10;i++)
+  {
+    for(int j=0;j<10;j++)
+    {
+      //if(p2Ships.checkHit(i+1,j+1) == "S" || 
+      //p2Ships.checkHit(i+1,j+1) == "P" ||
+      //p2Ships.checkHit(i+1,j+1) == "c" ||
+      //p2Ships.checkHit(i+1,j+1) == "D" ||
+      //p2Ships.checkHit(i+1,j+1) == "B" ||
+      //p2Ships.checkHit(i+1,j+1) == "C" ||)
+      background.draw_rectangle((W/12)+(W/12)*j+1,(H/12)*i+(H/6)+1, 
+      (W/12)*j+(W/6)-1, (H/12)*i+((3*H)/12)-1, attacked);
+      //else if(p2Ships.checkHit(i+1, j+1) == ".")
+      
+      background.draw_rectangle((W/12)+(W/12)*j,(H/12)*i+(H/6), 
+      (W/12)*j+(W/6), (H/12)*i+((3*H)/12), gridLines, 1, ~0U);
+    }
+  }
+  
+
+  //create display
+  CImgDisplay disp(background,"BattleShip",0,false,true);
+  disp.move((CImgDisplay::screen_width() - disp.width())/2,(CImgDisplay::screen_height() - disp.height())/2);
+  std::getchar();
         
 }
 
